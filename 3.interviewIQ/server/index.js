b83dda393de9a2f4953c375d8febc7f49b59bedb
Aman_Kumar <amankr91100@gmail.com>
2026-06-26 00:22:26 +0530

    Update CORS origin for deployment

--- 3.interviewIQ/server/index.js
+++ 3.interviewIQ/server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from "./routes/payment.route.js"
 const app = express()
 app.use(cors({
     // origin:"http://localhost:5173",
-    origin:"https://aiinterviewagent-iq.onrender.com",
+    origin:"https://aiinterviewagent-iq1.onrender.com",
     credentials:true
 }))
 
